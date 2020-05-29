37d9189f1a769d5e8afab8c458d30f4153f834ed
vinhtvph09066 <vinhtvph09066@fpt.edu.vn>
2020-05-29 21:36:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/agile_phoneshoping/fragment/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.agile_phoneshoping.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.agile_phoneshoping.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+
+}
