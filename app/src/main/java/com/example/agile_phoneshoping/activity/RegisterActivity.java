9357c98697aed33e68ef5282e2c9934a34da0ebf
vinhtvph09066 <vinhtvph09066@fpt.edu.vn>
2020-06-04 10:58:47 +0700

    test 2

---
+++ app/src/main/java/com/example/agile_phoneshoping/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.agile_phoneshoping.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agile_phoneshoping.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
