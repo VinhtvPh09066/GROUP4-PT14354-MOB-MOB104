a8baf9da81bb0ef9b1e6be905352be64485d4394
nguyen avn tu <tunvph08768@fpt.edu.vn>
2020-06-10 15:14:42 +0700

    tu update

--- Code2020/app/src/main/java/com/example/agile_phoneshoping/activity/LoginActivity.java
+++ Code2020/app/src/main/java/com/example/agile_phoneshoping/activity/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.example.agile_phoneshoping.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.example.agile_phoneshoping.R;
+import com.example.agile_phoneshoping.User;
+import com.example.agile_phoneshoping.database.AppDatabase;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,17 +23,25 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText edtPassword;
     private Button btnLogin;
     private TextView tvSignUp;
+    //tạo biến để lưu data
+    public  static  final  String SHARED_PREFS="SHAREDPREFS";
+    public  static  final  String USERNAME="text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initView();
-
+        AppDatabase db = Room.databaseBuilder(this,
+                AppDatabase.class, "user.db").allowMainThreadQueries().build();
+        //test data
+        db.userDAO().insert(new User(1,"nguyễn văn tú","tunvph",5555,"hà nam","tiền mặt","user"));
+//List<user> result=db.userDAO().getAll();
         // Login navigate
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                saveData();
                 if (validater()) {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
@@ -81,6 +94,18 @@ public class LoginActivity extends AppCompatActivity {
 
         return true;
     }
+    //save data
+    public void saveData(){
+        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFS, MODE_PRIVATE );
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString(USERNAME,edtUsername.getText().toString());
+        editor.apply();
 
-    ;
+//        editor.commit();
+        String userName=sharedPreferences.getString(USERNAME,"not found");
+        Log.e("user đăng nhập  là ", ":"+userName );
+        Toast.makeText(getApplicationContext(),"user đăng nhập  là "+userName,Toast.LENGTH_SHORT).show();
+
+
+    }
 }
